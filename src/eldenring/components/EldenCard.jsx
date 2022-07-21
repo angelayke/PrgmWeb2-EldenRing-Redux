edf0a64b09de3d9622b5a1ecaa2a6ea7ff8e95b2
angelayke <angebee2605@aol.com>
2022-07-21 20:41:10 +0200

    add Link

--- src/eldenring/components/EldenCard.jsx
+++ src/eldenring/components/EldenCard.jsx
@@ -2,27 +2,30 @@ import React from 'react';
 import Card from "../../components/Card";
 import BCard from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
 
 const DEFAULT_IMAGE_POSITION = "top";
 
 const EldenCard = ({eldenring}) => {
   return (
     <Col className="my-3">
-    <Card>
-      <BCard.Header as="small">{eldenring.location}</BCard.Header>
-      <BCard.Body>
-        <BCard.Img
-          variant={DEFAULT_IMAGE_POSITION}
-          src={eldenring.image} //a changer src={eldenring.data.other["official-artwork"].front_default} 
-          loading="lazy"
-          alt={eldenring.name} 
-        />
-        <BCard.Title>{eldenring.name}</BCard.Title>
-        <BCard.Text>{eldenring.description}</BCard.Text>
-      </BCard.Body>
-    </Card>
+    <Link to={`/eldenring/${eldenring.id}`}>
+      <Card>
+        <BCard.Header as="small">{eldenring.location}</BCard.Header>
+        <BCard.Body>
+          <BCard.Img
+            variant={DEFAULT_IMAGE_POSITION}
+            src={eldenring.image} //a changer src={eldenring.data.other["official-artwork"].front_default} 
+            loading="lazy"
+            alt={eldenring.name} 
+          />
+          <BCard.Title>{eldenring.name}</BCard.Title>
+          <BCard.Text>{eldenring.description}</BCard.Text>
+        </BCard.Body>
+      </Card>
+    </Link>
   </Col>
   )
 }
 
-export default EldenCard
+export default EldenCard;

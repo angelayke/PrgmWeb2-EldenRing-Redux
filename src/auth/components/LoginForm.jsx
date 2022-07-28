09e640f4ce061ae6311b3950b1c76ac4861557e3
angelayke <angebee2605@aol.com>
2022-07-28 21:09:03 +0200

    login form

---
+++ src/auth/components/LoginForm.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Input from "../../eldenring/components/Input";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
+import Input from "./Input";
+import Button from "react-bootstrap/Button";
+
+const LoginForm = () => {
+    // On crée un état qui contiendra un objet. Cet objet sera
+    // les valeurs initiales de notre formulaire
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        name: "",
+        lastname: "",
+    });
+
+    const onChange = (newValue, id) => {
+        setFormData({
+            ...formData,
+            // Ici c’est ce que nous appelons une dynamic key. L’id
+            // du input est passé à la fonction (voir component Input)
+            // puis cet id sera la clé passée à notre objet formulaire.
+            // La variable newValue sera la nouvelle valeur associée à
+            // la clé. On aura la clé ajoutée au state.
+            [id]: newValue,
+        });
+    };
+    const onSubmit = (event) => {
+         // Important pour que la page ne rafraîchisse pas
+         event.preventDefault();
+         // On fait notre logique au submit ici.
+        console.log(formData);
+    }
+        
+    return (
+        <Container>
+            <Container>
+                <Form onSubmit={onSubmit}>
+                    <Row>
+                        <Col>
+                            <Input id="email" onChange={onChange} value={formData.email} />
+                        </Col>
+                        <Col>
+                            <Input id="password" type="password" onChange={onChange} value={formData.email} />
+                        </Col>
+                        <Col>
+                            <Button type="submit">Connexion</Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Container>
+        </Container>
+    );
+    };
+    export default LoginForm;
--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,7 @@ import Container from "react-bootstrap/Container";
 import Eldenrings from '../eldenring/components/Eldenrings';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndividualEldenring from '../eldenring/components/IndividualEldenring';
-//verif ou je dois le mettre
-//const LoginForm = () => {
- // const [formData, setFormData] = useState({
-  //  email: "",
-  //  password: "",
-  //  name: "",
- //   lastname: "",
-//  });
 
-//  return (
-    //coupé pour etre plus bref
-//  ) 
-//}
 
 function App() {
   return (
